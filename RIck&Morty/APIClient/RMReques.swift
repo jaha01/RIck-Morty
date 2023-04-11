d2d672c43a670a58794626e0ba4353f6b95a36dc
jaha01 <anvarov94@gmail.com>
2023-04-11 23:33:27 +0300

    Lesson #2
    End of the lesson #2 - Succesfully

---
+++ RIck&Morty/APIClient/RMReques.swift
@@ -0,0 +1,13 @@
+//
+//  RMReques.swift
+//  RIck&Morty
+//
+//  Created by Jahongir Anvarov on 11.04.2023.
+//
+
+import Foundation
+
+/// Object that represents a sungle API Call
+final class RMRequest {
+    
+}
