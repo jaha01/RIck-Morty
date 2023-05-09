b6e2578dd90c41a9c5ae360ecc64b81ea94649da
jaha01 <anvarov94@gmail.com>
2023-05-09 21:20:07 +0300

    lesson #8
    end of the lesson #8 successfully

--- RIck&Morty/Views/RMCharacterListView.swift
+++ RIck&Morty/Views/RMCharacterListView.swift
@@ -40,6 +40,7 @@ final class RMCharacterListView: UIView {
         addSubviews(collectionView, spinner)
         addConstraints()
         spinner.startAnimating()
+        viewModel.delegate = self
         viewModel.fetchCharacters()
         setupCollectionView()
     }
@@ -64,15 +65,28 @@ final class RMCharacterListView: UIView {
     private func setupCollectionView() {
         collectionView.delegate = viewModel
         collectionView.dataSource = viewModel
-        
-        DispatchQueue.main.asyncAfter(deadline: .now()+2, execute: {
-            self.spinner.stopAnimating()
-            
-            self.collectionView.isHidden = false
-            
-            UIView.animate(withDuration: 0.4) {
-                self.collectionView.alpha = 1
-            }
-        })
+ 
+        //  эта часть была захардкожена
+//        DispatchQueue.main.asyncAfter(deadline: .now()+2, execute: {
+//            self.spinner.stopAnimating()
+//
+//            self.collectionView.isHidden = false
+//
+//            UIView.animate(withDuration: 0.4) {
+//                self.collectionView.alpha = 1
+//            }
+//        })
+    }
+}
+
+extension RMCharacterListView: RMCharacterListViewViewModelDelegate {
+    func didLoadInitialCharacters() {
+        collectionView.reloadData()
+        spinner.stopAnimating()
+        collectionView.isHidden = false
+        collectionView.reloadData() // Initial fetch
+        UIView.animate(withDuration: 0.4) {
+            self.collectionView.alpha = 1
+        }
     }
 }
