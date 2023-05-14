e8f7a29bf8bdb5702503791f8c8a0fd8978f2279
jaha01 <anvarov94@gmail.com>
2023-05-14 19:22:11 +0300

    lesson #18
    end of the lesson #18 successfully

--- RIck&Morty/Views/CharacterDetails/RMCharacterInfoCollectionViewCell.swift
+++ RIck&Morty/Views/CharacterDetails/RMCharacterInfoCollectionViewCell.swift
@@ -10,10 +10,46 @@ import UIKit
 final class RMCharacterInfoCollectionViewCell: UICollectionViewCell {
     static let cellIdentifier = "RMCharacterInfoCollectionViewCell"
     
+    private let valueLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "earth"
+        label.font = .systemFont(ofSize: 22, weight: .light)
+        return label
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "location"
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 20, weight: .medium)
+        return label
+    }()
+    
+    private let iconImageView: UIImageView = {
+        let icon = UIImageView()
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        icon.image = UIImage(systemName: "globe.americas")
+        icon.contentMode = .scaleAspectFit
+        return icon
+    }()
+    
+    private let titleContainer: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .tertiarySystemBackground
+        return view
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.backgroundColor = .tertiarySystemBackground
+        contentView.backgroundColor = .secondarySystemBackground
         contentView.layer.cornerRadius = 8
+        contentView.layer.masksToBounds = true
+        contentView.addSubviews(titleContainer, valueLabel, iconImageView)
+        titleContainer.addSubview(titleLabel)
+        setupConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -21,11 +57,36 @@ final class RMCharacterInfoCollectionViewCell: UICollectionViewCell {
     }
     
     private func setupConstraints() {
-        
+        NSLayoutConstraint.activate([
+            titleContainer.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+            titleContainer.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+            titleContainer.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            titleContainer.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.33),
+            
+            titleLabel.leftAnchor.constraint(equalTo: titleContainer.leftAnchor),
+            titleLabel.rightAnchor.constraint(equalTo: titleContainer.rightAnchor),
+            titleLabel.topAnchor.constraint(equalTo: titleContainer.topAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: titleContainer.bottomAnchor),
+            
+            iconImageView.heightAnchor.constraint(equalToConstant: 30),
+            iconImageView.widthAnchor.constraint(equalToConstant: 30),
+            iconImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 35),
+            iconImageView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 20),
+//            iconImageView.bottomAnchor.constraint(equalTo: titleContainer.topAnchor, constant: -10),
+
+            valueLabel.leftAnchor.constraint(equalTo: iconImageView.rightAnchor, constant: 10),
+            valueLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -10),
+            valueLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 36),
+           // valueLabel.bottomAnchor.constraint(equalTo: titleContainer.topAnchor, constant: -10),
+            valueLabel.heightAnchor.constraint(equalToConstant: 30)
+        ])
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
+//        valueLabel.text = nil
+//        titleLabel.text = nil
+//        iconImageView.image = nil
     }
     
     public func configure(with viewModel: RMCharacterInfoCollectionViewCellViewModel) {
